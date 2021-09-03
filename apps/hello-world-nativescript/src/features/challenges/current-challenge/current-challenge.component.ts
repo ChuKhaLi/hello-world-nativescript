3fdb56e6d90e2628f6855204d35c5e84d9f8abbd
ChungND <chungdinhnguyen.ukb@gmail.com>
2021-09-03 16:55:20 +0700

    change theme

--- apps/hello-world-nativescript/src/features/challenges/current-challenge/current-challenge.component.ts
+++ apps/hello-world-nativescript/src/features/challenges/current-challenge/current-challenge.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ObservableArray } from '@nativescript/core';
+import { ItemEventData, ObservableArray } from '@nativescript/core';
 
 @Component({
     selector: 'current-challenge',
     templateUrl: './current-challenge.component.html',
-    styleUrls: ['./current-challenge.component.css']
+    styleUrls: ['./current-challenge.component.scss']
 })
 export class CurrentChallengeComponent implements OnInit {
     challenges = new ObservableArray<string>();
@@ -16,8 +16,16 @@ export class CurrentChallengeComponent implements OnInit {
     }
 
     onSetChallenge() {
+        if (!this.currentChallenge || !this.currentChallenge.trim()) {
+            return;
+        }
+
         this.challenges.push(this.currentChallenge);
         this.currentChallenge = '';
+    }
+
+    onItemTap(args: ItemEventData) {
+        this.challenges.splice(args.index, 1);
     }
 
 }
